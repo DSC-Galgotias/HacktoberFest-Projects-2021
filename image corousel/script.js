588a3bebb47a9549b98216a11b4fb5d631150e9b
Sarvesh-Bhalerao <82701590+Sarvesh-Bhalerao@users.noreply.github.com>
2021-10-15 00:19:22 +0530

    uploaded scriptStyle

---
+++ image corousel/script.js
@@ -0,0 +1,35 @@
+"use strict";
+const leftbtn = document.querySelector("#left");
+const rightbtn = document.querySelector("#right");
+const imgs = document.getElementById("imgs");
+
+const img = document.querySelectorAll("#imgs img");
+
+let index = 0;
+
+let interval = setInterval(run, 4000);
+
+function run() {
+  index++;
+  changeimage();
+}
+
+function changeimage() {
+  if (index > img.length - 1) {
+    index = 0;
+  } else if (index < 0) {
+    index = img.length - 1;
+  }
+
+  imgs.style.transform = `translateX(${-index * 500}px)`;
+}
+
+leftbtn.addEventListener("click", () => {
+  index--;
+  changeimage();
+});
+
+rightbtn.addEventListener("click", () => {
+  index++;
+  changeimage();
+});
